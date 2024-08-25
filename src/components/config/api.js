ab36a87cd8550672368562eb29e7e3daef31921a
haritha75 <yerukonduharitha@gmail.com>
2024-08-25 13:36:45 +0530

    initial phase

--- src/components/config/api.js
+++ src/components/config/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL:
+    "https://taskmanagementspringboot-aahfeqggang5fdee.southindia-01.azurewebsites.net/api",
 });
 // https://revtask-hfenhja8emeyddbs.southindia-01.azurewebsites.net/api/
 export default api;
